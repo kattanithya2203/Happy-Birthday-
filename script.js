b363b0dff933e0e5c5c254b6af5a1c2c25b7d186
kattanithya2203 <kattanithya2203@gmail.com>
2026-08-01 02:55:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function openEnvelope() {
 
+document.getElementById("envelope-section").style.display = "none";
+
+document.getElementById("card-section").style.display = "flex";
+
+}
+
+function openCard() {
+
+document.querySelector(".front").style.display = "none";
+
+document.querySelector(".inside").style.display = "block";
+
+}
